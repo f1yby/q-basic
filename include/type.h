b0f72ba9f4e60943523b88f34375210827373b11
Jiarui Wang <wangjiarui4@outlook.com>
2022-12-05 22:37:03 +0800

    feat(type): add Map as std::map

--- include/type.h
+++ include/type.h
@@ -1,10 +1,14 @@
 #pragma once
+#include <map>
 #include <memory>
 #include <string>
 #include <vector>
 
 template <typename T>
 using Vec = std::vector<T>;
+
+template <typename K, typename V>
+using Map = std::map<K, V>;
 
 template <typename T>
 using Rc = std::shared_ptr<T>;
